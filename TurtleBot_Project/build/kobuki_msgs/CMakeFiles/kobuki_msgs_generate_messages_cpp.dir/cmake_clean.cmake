b84881e78a3252bb1998b08781533a1b9d034dbd
luislechugaruiz <luislechugaruiz@gmail.com>
2020-02-19 19:16:54 +0100

    Update

---
+++ TurtleBot_Project/build/kobuki_msgs/CMakeFiles/kobuki_msgs_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kobuki_msgs_generate_messages_cpp"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/ExternalPower.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/RobotStateEvent.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/KeyboardInput.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/SensorState.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/AutoDockingAction.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/AutoDockingActionResult.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/ScanAngle.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/CliffEvent.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/ButtonEvent.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/AutoDockingFeedback.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/ControllerInfo.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/Led.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/AutoDockingGoal.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/DockInfraRed.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/BumperEvent.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/DigitalInputEvent.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/VersionInfo.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/WheelDropEvent.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/AutoDockingResult.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/AutoDockingActionGoal.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/MotorPower.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/AutoDockingActionFeedback.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/Sound.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/DigitalOutput.h"
+  "/home/luis/TurtleBot_Project/devel/include/kobuki_msgs/PowerSystemEvent.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/kobuki_msgs_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
