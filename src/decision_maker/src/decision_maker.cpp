a1a288bd47aae839ae92f04c931f1ce4f52ff614
luislechugaruiz <luislechugaruiz@gmail.com>
2020-03-23 15:56:12 +0100

    Basic State machine

--- src/decision_maker/src/decision_maker.cpp
+++ src/decision_maker/src/decision_maker.cpp
@@ -27,18 +27,30 @@ void Decision::ROI_callBack(poi_database::ROI New_ROI)
       if(type == "P") database_ptr = &database_p;
       int index_ptr = database_ptr->size();
       database_ptr->push_back(New_ROI);
-      received_ROIs.push_back(&database_ptr->at(index_ptr));
-      new_roi_received = true;
+      //received_ROIs.push_back(&database_ptr->at(index_ptr));
+      //Now we got a pointer to the ith of database_p and a inital cost inside of the vector in a pair!
+      if(type == "P"){
+        cost_p_not_rescued.push_back(std::make_pair(database_ptr->at(index_ptr), initial_cost));
+      }
     }
-    else
-    {
-      received_ROIs.at(New_ROI.index)->size_x = New_ROI.size_x;
-      received_ROIs.at(New_ROI.index)->size_y = New_ROI.size_y;
-      received_ROIs.at(New_ROI.index)->center = New_ROI.center;
-    }
+
+    //else if(!ROI_isEmpty(received_ROIs.at(New_ROI.index)))
+    //{
+    //  ROS_INFO("DENTRO");
+    //  std::string _type = received_ROIs.at(New_ROI.index)->type;
+    //  ROS_INFO("Modifying ROI type: %s", _type.c_str());
+    //  ROS_INFO("Antes centro x: %f", received_ROIs.at(New_ROI.index)->center.x);
+    //  ROS_INFO("Antes centro y: %f", received_ROIs.at(New_ROI.index)->center.y);
+    //  received_ROIs.at(New_ROI.index)->size_x = New_ROI.size_x;
+    //  received_ROIs.at(New_ROI.index)->size_y = New_ROI.size_y;
+    //  received_ROIs.at(New_ROI.index)->center = New_ROI.center;
+    //  ROS_INFO("Ahora centro x: %f", received_ROIs.at(New_ROI.index)->center.x);
+    //  ROS_INFO("Ahora centro y: %f", received_ROIs.at(New_ROI.index)->center.y);
+    //}
 }
 
-void Decision::doneCb(const actionlib::SimpleClientGoalState& state)
+//This is the only function that sets isMoving to false
+void Decision::moving_done_Callback(const actionlib::SimpleClientGoalState& state)
 {
     if (state == actionlib::SimpleClientGoalState::SUCCEEDED)
     {
@@ -47,6 +59,7 @@ void Decision::doneCb(const actionlib::SimpleClientGoalState& state)
     }
 }
 
+//WORKING GOOD
 void Decision::getActualPose()
 {
   tf::StampedTransform transform;
@@ -62,67 +75,160 @@ void Decision::getActualPose()
   actualPose.pose.orientation.w = transform.getRotation().w();
 }
 
-void Decision::setGoalPose(float posx, float posy, float orientationz, float orientationw)
+void Decision::setGoalPose(poi_database::ROI ROI)
 {
+  //. o ->? pasando como referencia es un pointer o simplemente el objeto?
   goalPose.header.stamp = ros::Time::now();
   goalPose.header.frame_id = "map";
-  goalPose.pose.position.x = posx;
-  goalPose.pose.position.y = posy;
-  goalPose.pose.orientation.z = orientationz;
-  goalPose.pose.orientation.w = orientationw;
+  if(ROI.size_x > ROI.size_y)
+  {
+    goalPose.pose.position.x = ROI.center.x;
+    goalPose.pose.position.y = ROI.center.y - 1;
+    goalPose.pose.orientation.z = 0.707;
+    goalPose.pose.orientation.w = 0.707;
+  }
+  else{
+    goalPose.pose.position.x = ROI.center.x - 1;
+    goalPose.pose.position.y = ROI.center.y;
+    goalPose.pose.orientation.w = 1;
+  }
 }
+
+void Decision::setGreedyAction(bool state)
+{
+  greedy.greedy = state;
+  acGreedy.sendGoal(greedy);
+}
+
+int Decision::getCost()
+{
+  cost_request.request.start = actualPose;
+  cost_request.request.goal = goalPose;
+  cost_client.call(cost_request);
+  return cost_request.response.cost;
+}
+
+bool Decision::sortbysec(const std::pair<poi_database::ROI,int> &a, const std::pair<poi_database::ROI,int> &b)
+{
+    return (a.second < b.second);
+}
+
+void Decision::findNearestPerson()
+{
+  getActualPose();
+  for(auto& pair_p_cost : cost_p_not_rescued)
+  {
+    setGoalPose(pair_p_cost.first);
+    pair_p_cost.second = getCost();
+    ROS_INFO("Dist x = %f", pair_p_cost.first.center.x - actualPose.pose.position.x);
+    ROS_INFO("Dist y = %f", pair_p_cost.first.center.y - actualPose.pose.position.y);
+    ROS_INFO("Cost = %d", pair_p_cost.second);
+  }
+  std::sort(cost_p_not_rescued.begin(), cost_p_not_rescued.end(), sortbysec);
+  persons_not_rescued = cost_p_not_rescued.size();
+}
+
+void Decision::callMoveAction()
+{
+  goal.target_pose = goalPose;
+  acMove.sendGoal(goal, boost::bind(&Decision::moving_done_Callback, this, _1));
+}
+
+bool Decision::ROI_isEmpty(poi_database::ROI* ROI)
+{
+  ROS_INFO("Checking if empty");
+  return (ROI->size_x == 1000);
+}
+
 
 void Decision::process()
 {
-
-  if (i < received_ROIs.size() && !isMoving)
+//Three states:
+// searching_exit   -> Initially it would just look for the exit (moving greedy)
+// rescuing         -> Once we found it start rescuing persons if there is anynone in the vector (cost_p_not_rescued)
+// searching_person -> In case we already rescued anyone on the list search persons again and if one is found return to rescue state
+// finished         -> If all the persons have been rescued then finish
+  switch(_state)
   {
-    //Set Greedy to false so we can control the move action
-    if(greedy.greedy)
-    {
-      ROS_INFO("Greedy False");
-      greedy.greedy = false;
-      acGreedy.sendGoal(greedy);
-    }
+    case _state_searching_exit:
+      if (database_e.size() == 0)
+      {
+        if(!greedy.greedy)
+        {
+          ROS_INFO("State 0: GOING GREEDY SEARCHING EXIT");
+          setGreedyAction(true);
+        }
+      }
+      else if (cost_p_not_rescued.size() > 0)
+      {
+        ROS_INFO("State 1: GOING TO RESCUE NEAREST PERSON");
+        _state = _state_rescuing;
+        setGreedyAction(false);
+      }
+      else
+      {
+        _state = _state_searching_person;
+      }
+    break;
 
-    getActualPose();
+    case _state_rescuing:
+      //move to the person
+      if (!isMoving && !isgoing_to_person)
+      {
+        //check if finished
+        if(persons_rescued != number_of_persons)
+        {
+          //check if there is any person to rescue in the queue
+          if(cost_p_not_rescued.size() > 0)
+          {
+            ROS_INFO("RESCUING!");
+            ROS_INFO("PERSONS TO RESCUE: %d", persons_not_rescued);
+            //Recalculate as new routes could be seen and cost modified
+            findNearestPerson();
+            setGoalPose(cost_p_not_rescued[0].first);
+            ROS_INFO("ROI center MENOR x = %f", cost_p_not_rescued[0].first.center.x);
+            ROS_INFO("ROI center MENOR y = %f", cost_p_not_rescued[0].first.center.y);
+            ROS_INFO("Cost = %d", cost_p_not_rescued[0].second);
+            callMoveAction();
+            isMoving = true;
+            isgoing_to_person = true;
+          }
+          else
+          {
+            ROS_INFO("State 2: GOING GREEDY SEARCHING PERSON");
+            _state = _state_searching_person;
+            setGreedyAction(true);
+          }
+        }
+        else _state = _state_finished;
+      }
+      //if not moving and was going to the person we already reached the goal!
+      else if (!isMoving && isgoing_to_person)
+      {
+        setGoalPose(database_e[0]);
+        callMoveAction();
+        //set at NULL the person at database_p
+        //cost_p_not_rescued.at(0).first.size_x = 1000;
+        //erase the person rescued
+        cost_p_not_rescued.erase(cost_p_not_rescued.begin());
+        persons_not_rescued--;
+        persons_rescued++;
+        isMoving = true;
+        isgoing_to_person = false;
+      }
+    break;
 
-    //if is in vertical position rotate the final orientation to look frontly the ROI
-    if(received_ROIs.at(i)->size_x > received_ROIs.at(i)->size_y)
-    {
+    case _state_searching_person:
+      if (cost_p_not_rescued.size() > 0)
+      {
+        _state = _state_rescuing;
+        setGreedyAction(false);
+      }
+    break;
 
-      setGoalPose(received_ROIs.at(i)->center.x, received_ROIs.at(i)->center.y - 1, 0.707, 0.707);
-    }
-    else
-    {
-      setGoalPose(received_ROIs.at(i)->center.x - 1, received_ROIs.at(i)->center.y, 0, 1);
-    }
-
-    cost_request.request.start = actualPose;
-    cost_request.request.goal = goalPose;
-    goal.target_pose = goalPose;
-    cost_client.call(cost_request);
-
-    //prueba
-    int cost = cost_request.response.cost;
-    std::string type2 = received_ROIs.at(i)->type;
-    ROS_INFO("Moving to type: %s", type2.c_str());
-    ROS_INFO("Pos x: %f", received_ROIs.at(i)->center.x);
-    ROS_INFO("Pos y: %f", received_ROIs.at(i)->center.y);
-    ROS_INFO("cost : %d", cost);
-
-    acMove.sendGoal(goal, boost::bind(&Decision::doneCb, this, _1));
-    isMoving = true;
-    i++;
-  }
-
-  else if(!isMoving)
-  {
-    if(!greedy.greedy)
-    {
-      ROS_INFO("Greedy");
-      greedy.greedy = true;
-      acGreedy.sendGoal(greedy);
-    }
+    case _state_finished:
+      ROS_INFO("congratulations, you are a f**** beast");
+      ros::Duration(10).sleep();
+    break;
   }
 }
--- src/decision_maker/src/decision_maker.h
+++ src/decision_maker/src/decision_maker.h
@@ -32,22 +32,45 @@ class Decision{
     MoveBaseClient acMove;
     ExploreGreedyClient acGreedy;
 
+    static bool sortbysec(const std::pair<poi_database::ROI,int> &a, const std::pair<poi_database::ROI,int> &b);
+
+    bool ROI_isEmpty(poi_database::ROI* ROI);
+
+    void findNearestPerson();
+
     void getActualPose();
 
-    void setGoalPose(float posx, float posy, float orientationz, float orientationw);
+    int getCost();
+
+    void setGoalPose(poi_database::ROI ROI);
+
+    void setGreedyAction(bool state);
+
+    void callMoveAction();
 
     void ROI_callBack(poi_database::ROI);
 
-    void doneCb(const actionlib::SimpleClientGoalState& state);
+    void moving_done_Callback(const actionlib::SimpleClientGoalState& state);
 
   private:
 
+    //STATES
+    static const int _state_searching_exit = 0;
+    static const int _state_rescuing = 1;
+    static const int _state_searching_person = 2;
+    static const int _state_finished = 3;
+    int _state = 0;
+
+
+    const int number_of_persons = 5;
+    const int initial_cost = 1000000000;
+    int persons_not_rescued = 0;
+    int persons_rescued = 0;
+
     std::string type;
 
-    bool new_roi_received = false;
     bool isMoving = false;
-
-    int i = 0;
+    bool isgoing_to_person = false;
 
     geometry_msgs::PoseStamped actualPose;
     geometry_msgs::PoseStamped goalPose;
@@ -56,11 +79,13 @@ class Decision{
     explore_lite::greedyGoal greedy;
     turtlebot_2dnav::returnCost cost_request;
 
-    std::vector<poi_database::ROI *> received_ROIs;
     std::vector<poi_database::ROI> database_r;
     std::vector<poi_database::ROI> database_e;
     std::vector<poi_database::ROI> database_p;
     std::vector<poi_database::ROI> *database_ptr;
+    std::vector<poi_database::ROI*> received_ROIs;
+    std::vector<std::pair<poi_database::ROI,int>> cost_p_not_rescued;
+    //std::vector<poi_database::ROI> already_rescued_person;
 
 
 };
