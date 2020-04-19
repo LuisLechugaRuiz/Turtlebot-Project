2ec8144f8c3606dbfc4f7c8eb6c5f41c0592962b
luislechugaruiz <luislechugaruiz@gmail.com>
2020-04-19 18:19:28 +0200

    devel

--- devel/share/common-lisp/ros/turtlebot_2dnav/srv/_package_clearCostmap.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package turtlebot_2dnav-srv)
-(cl:export '(CLEAR-VAL
-          CLEAR
-          RECEIVED-VAL
-          RECEIVED
-))
--- devel/share/costmap_restrictor/cmake/costmap_restrictor-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(costmap_restrictor_MSG_INCLUDE_DIRS "/home/luis/Turtlebot-Project/src/costmap_restrictor/msg")
-set(costmap_restrictor_MSG_DEPENDENCIES costmap_restrictor/fake_bound;std_msgs;geometry_msgs)
